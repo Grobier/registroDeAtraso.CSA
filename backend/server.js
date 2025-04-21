dab21ff559e4c626de9aa3330ec31037504efd88
LorenzoGrobier <grobier.2h@gmail.com>
2025-04-21 08:22:44 -0400

    RegistroDeatraso v2.7

--- backend/server.js
+++ backend/server.js
@@ -28,10 +28,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 // Conexión a MongoDB usando Mongoose
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
+mongoose.connect(process.env.MONGO_URI,)
 .then(() => console.log("Conectado a MongoDB con Mongoose"))
 .catch(err => console.error("Error de conexión:", err));
 
